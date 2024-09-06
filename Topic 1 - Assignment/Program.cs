438d30b469bd90b0e1534ce6f773e676d3c91b12
Emmett Cornies <339761330@GoTVDSB.ca>
2024-09-06 08:58:35 -0400

    Done

--- Topic 1 - Assignment/Program.cs
+++ Topic 1 - Assignment/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello there, this is Emmett");
+Console.WriteLine("");
+Console.WriteLine("We use comments to add explanations to code to remind yourself about what you were doing in that specific line, or to tell your friends what that code does.");
+Console.WriteLine("");
+Console.WriteLine("EEEEE  CCC");
+Console.WriteLine("E     C   C");
+Console.WriteLine("E     C");
+Console.WriteLine("EEE   C");
+Console.WriteLine("E     C");
+Console.WriteLine("E     C   C");
+Console.WriteLine("EEEEE  CCC");
+
